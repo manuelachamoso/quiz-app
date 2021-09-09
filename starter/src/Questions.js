84eea1a720b1ae458741ea01b92bd3536c225788
Manuela Chamoso <manuela.chamoso@gmail.com>
2021-09-09 20:52:46 +0200

    first commit

---
+++ starter/src/Questions.js
@@ -0,0 +1,93 @@
+const questions = [
+    {
+        questionText: 'What is the capital of France?',
+        answerOptions: [
+            { answerText: 'New York', isCorrect: false },
+            { answerText: 'London', isCorrect: false },
+            { answerText: 'Paris', isCorrect: true },
+            { answerText: 'Dublin', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Who is CEO of Tesla?',
+        answerOptions: [
+            { answerText: 'Jeff Bezos', isCorrect: false },
+            { answerText: 'Elon Musk', isCorrect: true },
+            { answerText: 'Bill Gates', isCorrect: false },
+            { answerText: 'Tony Stark', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'The iPhone was created by which company?',
+        answerOptions: [
+            { answerText: 'Apple', isCorrect: true },
+            { answerText: 'Intel', isCorrect: false },
+            { answerText: 'Amazon', isCorrect: false },
+            { answerText: 'Microsoft', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'How many Harry Potter books are there?',
+        answerOptions: [
+            { answerText: '1', isCorrect: false },
+            { answerText: '4', isCorrect: false },
+            { answerText: '6', isCorrect: false },
+            { answerText: '7', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'Which company developed JavaScript?',
+        answerOptions: [
+            { answerText: 'Facebook', isCorrect: false },
+            { answerText: 'Oracle Corporation', isCorrect: false },
+            { answerText: 'Netscape', isCorrect: true },
+            { answerText: 'Microsoft', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'What is the world`s longest river?',
+        answerOptions: [
+            { answerText: 'Nilo', isCorrect: true },
+            { answerText: 'Amazon', isCorrect: false },
+            { answerText: 'Mississippi', isCorrect: false },
+            { answerText: 'Rio de la Plata', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Which country is Prague in?',
+        answerOptions: [
+            { answerText: 'Netherlands', isCorrect: false },
+            { answerText: 'Poland', isCorrect: false },
+            { answerText: 'Czech Republic', isCorrect: true },
+            { answerText: 'Germany', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Who played Neo in The Matrix?',
+        answerOptions: [
+            { answerText: 'Keanu Reeves', isCorrect: true },
+            { answerText: 'Tom Hanks', isCorrect: false },
+            { answerText: 'Johnny Depp', isCorrect: false },
+            { answerText: 'Heath Ledger', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'What are the three primary colours?',
+        answerOptions: [
+            { answerText: 'Red, yellow and green', isCorrect: false },
+            { answerText: 'Red, yellow and blue', isCorrect: true },
+            { answerText: 'Blue, yellow and green', isCorrect: false },
+            { answerText: 'Black, white and red', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'When was the euro introduced as legal currency on the world market?',
+        answerOptions: [
+            { answerText: '2005', isCorrect: false },
+            { answerText: '2001', isCorrect: false },
+            { answerText: '2010', isCorrect: false },
+            { answerText: '1999', isCorrect: true },
+        ],
+    },
+];
+export default questions;
